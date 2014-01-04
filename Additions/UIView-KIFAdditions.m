392a5b97a2828958ff0144a177450666fb9d9ff7
pkania <pkania@belmonttechinc.com>
2014-01-04 14:10:49 -0500

    Don't warn about use of private selector in test code.

--- Additions/UIView-KIFAdditions.m
+++ Additions/UIView-KIFAdditions.m
@@ -521,7 +521,11 @@ typedef struct __GSEvent * GSEventRef;
 
 - (UIEvent *)_eventWithTouch:(UITouch *)touch;
 {
+  // _touchesEvent is a private selector, but we can ignore the warning because this is only used for testing.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
     UIEvent *event = [[UIApplication sharedApplication] _touchesEvent];
+#pragma clang diagnostic pop
     
     CGPoint location = [touch locationInView:touch.window];
     KIFEventProxy *eventProxy = [[KIFEventProxy alloc] init];
